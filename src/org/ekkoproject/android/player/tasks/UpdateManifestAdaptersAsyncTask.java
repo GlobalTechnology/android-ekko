e8b14d57eec21e41c76d1706a856b62dbece7e80
Daniel Frett <daniel.frett@ccci.org>
2013-06-13 22:30:43 -0400

    updated manifest update task to support multiple adapters

---
+++ src/org/ekkoproject/android/player/tasks/UpdateManifestAdaptersAsyncTask.java
@@ -0,0 +1,22 @@
+package org.ekkoproject.android.player.tasks;
+
+import org.ekkoproject.android.player.adapter.ManifestAdapter;
+import org.ekkoproject.android.player.model.Manifest;
+import org.ekkoproject.android.player.services.ManifestManager;
+
+public class UpdateManifestAdaptersAsyncTask extends UpdateManifestAsyncTask {
+    private final ManifestAdapter<?>[] adapters;
+
+    public UpdateManifestAdaptersAsyncTask(final ManifestManager manager, final ManifestAdapter<?>... adapters) {
+        super(manager);
+        this.adapters = adapters;
+    }
+
+    @Override
+    protected void onPostExecute(final Manifest result) {
+        super.onPostExecute(result);
+        for (final ManifestAdapter<?> adapter : this.adapters) {
+            adapter.swapManifest(result);
+        }
+    }
+}
