2e665f3c877efe132c92d7873aab3e3349b6d755
Daniel Frett <daniel.frett@ccci.org>
2013-06-24 01:35:16 -0400

    cleaned up the project dir by removing a bunch of unused files

--- src/org/appdev/CustomVideoPlayer/CustomVideoViewOnStopCallback.java
+++
@@ -1,7 +0,0 @@
-package org.appdev.CustomVideoPlayer;
-
-public interface CustomVideoViewOnStopCallback {
-	public void OnStop(int currentPlayPosition);
-	public void OnPlay(int currentPlayPosition);
-	public void OnVideoPaused(int currentPlayPosition);
-}
