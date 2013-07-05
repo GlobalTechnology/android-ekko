73f17a3eaa211d0e182fc08b9be2e910a11da185
Daniel Frett <daniel.frett@ccci.org>
2013-07-05 19:03:55 -0400

    add a course completion fragment

--- src/org/ekkoproject/android/player/adapter/ManifestContentPagerAdapter.java
+++ src/org/ekkoproject/android/player/adapter/ManifestContentPagerAdapter.java
@@ -10,6 +10,7 @@ import org.ekkoproject.android.player.model.Lesson;
 import org.ekkoproject.android.player.model.Manifest;
 import org.ekkoproject.android.player.model.Quiz;
 import org.ekkoproject.android.player.support.v4.fragment.AbstractContentFragment;
+import org.ekkoproject.android.player.support.v4.fragment.CourseCompletionFragment;
 import org.ekkoproject.android.player.support.v4.fragment.LessonFragment;
 import org.ekkoproject.android.player.support.v4.fragment.QuizFragment;
 
@@ -37,34 +38,39 @@ public class ManifestContentPagerAdapter extends AbstractManifestPagerAdapter {
 
     @Override
     public int getCount() {
-        return this.content.size();
+        return this.content.size() + 1;
     }
 
     @Override
     public Fragment getItem(final int position) {
         final Manifest manifest = this.getManifest();
         final long courseId = manifest != null ? manifest.getCourseId() : INVALID_COURSE;
-        final CourseContent item = this.content.get(position);
-
-        final Fragment fragment;
-        if (item instanceof Lesson) {
-            fragment = LessonFragment.newInstance(courseId, item.getId());
-        } else if (item instanceof Quiz) {
-            fragment = QuizFragment.newInstance(courseId, item.getId());
+        if (position >= this.content.size()) {
+            return CourseCompletionFragment.newInstance(courseId);
         } else {
-            fragment = null;
+            final CourseContent item = this.content.get(position);
+
+            final Fragment fragment;
+            if (item instanceof Lesson) {
+                fragment = LessonFragment.newInstance(courseId, item.getId());
+            } else if (item instanceof Quiz) {
+                fragment = QuizFragment.newInstance(courseId, item.getId());
+            } else {
+                fragment = null;
+            }
+            return fragment;
         }
-        return fragment;
     }
 
     @Override
     public int getItemPosition(final Object fragment) {
         // get the contentId represented by the fragment
         final String contentId;
-        if (fragment instanceof AbstractContentFragment) {
+        if (fragment instanceof CourseCompletionFragment) {
+            return this.content.size();
+        } else if (fragment instanceof AbstractContentFragment) {
             contentId = ((AbstractContentFragment) fragment).getContentId();
-        } else
-        if (fragment instanceof LessonFragment) {
+        } else if (fragment instanceof LessonFragment) {
             contentId = ((LessonFragment) fragment).getLessonId();
         } else {
             contentId = null;
---
+++ src/org/ekkoproject/android/player/support/v4/fragment/CourseCompletionFragment.java
@@ -0,0 +1,105 @@
+package org.ekkoproject.android.player.support.v4.fragment;
+
+import java.util.Set;
+
+import org.ekkoproject.android.player.R;
+import org.ekkoproject.android.player.model.Manifest;
+import org.ekkoproject.android.player.services.ProgressManager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class CourseCompletionFragment extends AbstractContentFragment implements View.OnClickListener {
+    private TextView completionMessageView = null;
+    private View courseListButton = null;
+
+    public static CourseCompletionFragment newInstance(final long courseId) {
+        final CourseCompletionFragment fragment = new CourseCompletionFragment();
+
+        // handle arguments
+        fragment.setArguments(buildArgs(courseId));
+
+        return fragment;
+    }
+
+    /* BEGIN lifecycle */
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_course_completion, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        this.findViews();
+        this.setupButtons();
+    }
+
+    @Override
+    protected void onManifestUpdate(final Manifest manifest) {
+        super.onManifestUpdate(manifest);
+        this.updateCompletionMessage(manifest);
+    }
+
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()) {
+        case R.id.course_list:
+            getActivity().onBackPressed();
+            break;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        this.clearViews();
+    }
+
+    /* END lifecycle */
+
+    private void findViews() {
+        this.completionMessageView = findView(TextView.class, R.id.completionMessage);
+        this.courseListButton = findView(View.class, R.id.course_list);
+    }
+
+    private void clearViews() {
+        this.completionMessageView = null;
+        this.courseListButton = null;
+    }
+
+    private void setupButtons() {
+        if (this.courseListButton != null) {
+            this.courseListButton.setOnClickListener(this);
+        }
+    }
+
+    private void updateCompletionMessage(final Manifest manifest) {
+        if (this.completionMessageView != null) {
+            String message = null;
+            String title = null;
+            if (manifest != null) {
+                message = manifest.getCompletionMessage();
+                title = manifest.getCourseTitle();
+            }
+
+            if (message == null || message.length() == 0) {
+                this.completionMessageView.setText(getResources().getString(R.string.course_completion_message_default,
+                        title));
+            } else {
+                this.completionMessageView.setText(Html.fromHtml(message));
+            }
+        }
+    }
+
+    @Override
+    protected Pair<Integer, Integer> getProgress(final Manifest manifest, final Set<String> progress) {
+        return ProgressManager.getCourseProgress(manifest, progress);
+    }
+}
