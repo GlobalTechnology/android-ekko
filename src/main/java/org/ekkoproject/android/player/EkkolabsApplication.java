44416508a07f76e021fae5dbec8ad5d11a3802b4
Daniel Frett <daniel.frett@ccci.org>
2014-07-21 11:40:47 -0400

    [#EKKOAND-45] kill the splash screen

--- src/main/java/org/ekkoproject/android/player/EkkolabsApplication.java
+++ src/main/java/org/ekkoproject/android/player/EkkolabsApplication.java
@@ -2,12 +2,18 @@ package org.ekkoproject.android.player;
 
 import static org.ekkoproject.android.player.BuildConfig.ARCLIGHT_API_KEY;
 import static org.ekkoproject.android.player.BuildConfig.NEW_RELIC_API_KEY;
+import static org.ekkoproject.android.player.BuildConfig.THEKEY_CLIENTID;
+import static org.ekkoproject.android.player.Constants.GUID_GUEST;
 
 import android.app.Application;
 import android.content.pm.PackageManager;
 
 import com.jesusfilmmedia.eventtracker.EventTracker;
 import com.newrelic.agent.android.NewRelic;
+
+import org.ekkoproject.android.player.sync.EkkoSyncService;
+
+import me.thekey.android.lib.TheKeyImpl;
 
 public class EkkolabsApplication extends Application {
     @Override
@@ -19,6 +25,10 @@ public class EkkolabsApplication extends Application {
 
         // Initialize Arclight Event Tracker
         EventTracker.getInstance().initialize(this, ARCLIGHT_API_KEY, getPackageName(), getAppVersionName());
+
+        // start an initial background sync
+        final String guid = TheKeyImpl.getInstance(this, THEKEY_CLIENTID).getGuid();
+        EkkoSyncService.syncCourses(this, guid != null ? guid : GUID_GUEST);
     }
 
     private String getAppVersionName() {
