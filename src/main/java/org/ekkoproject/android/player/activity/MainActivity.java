7503de561f85470032bc1fbe3ef45d61a202c5b6
Daniel Frett <daniel.frett@ccci.org>
2014-08-18 15:03:55 -0400

    make the MainActivity an ActionBarActivity to improve launch experience

--- src/main/java/org/ekkoproject/android/player/activity/MainActivity.java
+++ src/main/java/org/ekkoproject/android/player/activity/MainActivity.java
@@ -1,9 +1,9 @@
 package org.ekkoproject.android.player.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(final Bundle savedState) {
         super.onCreate(savedState);
